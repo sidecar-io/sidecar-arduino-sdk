2745e9d2255da0a147d2143831a4d5f28f5fd35f
sptrakesh <rakesh@sptci.com>
2015-08-18 19:55:19 -0500

    Added untracked files

---
+++ libraries/QSense/HttpRequest.cpp
@@ -0,0 +1,94 @@
+#include "HttpRequest.h"
+
+#if defined( ARDUINO )
+#include "../StandardCplusplus/cstdio"
+#include "../StandardCplusplus/sstream"
+#else
+#include <cstdio>
+#include <sstream>
+#endif
+
+using qsense::QString;
+using qsense::net::HttpRequest;
+
+const QString HttpRequest::ILLEGAL = "%<>{}|\\\"^`";
+
+HttpRequest::HttpRequest( const QString& path ) :
+  uri( path ), body(), parameters(), headers() {}
+
+
+HttpRequest& HttpRequest::setBody( const QString& txt )
+{
+  body = txt;
+  return *this;
+}
+
+
+HttpRequest& HttpRequest::setParameter( const QString& key, const QString& value )
+{
+  Map::iterator iter = parameters.find( key );
+  if ( iter != parameters.end() ) parameters.erase( iter );
+  parameters.insert( std::pair<QString,QString>( key, value ) );
+  return *this;
+}
+
+
+HttpRequest& HttpRequest::setHeader( const QString& key, const QString& value )
+{
+  Map::iterator iter = headers.find( key );
+  if ( iter != headers.end() ) headers.erase( iter );
+  headers.insert( std::pair<QString,QString>( key, value ) );
+  return *this;
+}
+
+
+const QString HttpRequest::getParamters() const
+{
+  std::stringstream ss;
+
+  bool first = true;
+  for ( Iterator iter = parameters.begin(); iter != parameters.end(); ++iter )
+  {
+    if ( ! first ) ss << "&";
+    ss << iter->first << "=" << encode( iter->second );
+    first = false;
+  }
+
+  return ss.str();
+}
+
+
+void HttpRequest::toHex( unsigned value, int width, QString& output ) const
+{
+  char buffer[64];
+  std::sprintf( buffer, "%0*X", width, value );
+  output.append( buffer );
+}
+
+
+QString HttpRequest::encode( const QString& str ) const
+{
+  QString encodedStr;
+
+  for ( QString::const_iterator it = str.begin(); it != str.end(); ++it )
+  {
+    char c = *it;
+    if ( ( c >= 'a' && c <= 'z' ) ||
+        ( c >= 'A' && c <= 'Z' ) ||
+        ( c >= '0' && c <= '9' ) ||
+        c == '-' || c == '_' ||
+        c == '.' || c == '~' )
+    {
+      encodedStr += c;
+    }
+    else if ( c <= 0x20 || c >= 0x7F ||
+      ILLEGAL.find(c) != QString::npos || '#' == c )
+    {
+      encodedStr += '%';
+      toHex( (unsigned) (unsigned char) c, 2, encodedStr );
+    }
+    else encodedStr += c;
+  }
+
+  return encodedStr;
+}
---
+++ libraries/QSense/HttpRequest.h
@@ -0,0 +1,86 @@
+#ifndef QSENSE_NET_HTTPREQUEST_H
+#define QSENSE_NET_HTTPREQUEST_H
+
+#if defined( ARDUINO )
+#include "QSense.h"
+#include "../StandardCplusplus/map"
+#else
+#include <QSense.h>
+#include <map>
+#endif
+
+namespace qsense
+{
+  namespace net
+  {
+    /**
+     * @brief A simple class that represents a HTTP request.  Request
+     * encapsulates the URI path, any request parameters, header attributes,
+     * body etc. as appropriate.
+     */
+    class HttpRequest
+    {
+    public:
+      /// Map used to represent request parameters and headers.
+      typedef std::map<QString,QString> Map;
+
+      /// Constant iterator to access contents of the parameters and headers
+      typedef Map::const_iterator Iterator;
+
+      /// Constructor.  Create a request for the specified server resource
+      HttpRequest( const QString& uri );
+
+      /// Destructor.  No actions required.
+      ~HttpRequest() {}
+
+      /// Return the uri for which this request was created.
+      const QString& getUri() const { return uri; }
+
+      /// Return the body to send as part of the request.
+      /// For GET requests, this will be empty.
+      const QString& getBody() const { return body; }
+
+      /// Set the body to send as part of the request.  This is meant
+      /// for use primarily with POST/PUT type requests.
+      HttpRequest& setBody( const QString& body );
+
+      /// Add the specified key/value combination as a request parameter
+      /// to this request.  If a mapping already exists with the specified
+      /// key, it will be replaced with the specified value.
+      HttpRequest& setParameter( const QString& key, const QString& value );
+
+      /// Add the specified key/value combination as a request attribute
+      /// to this request.  If a mapping already exists with the specified
+      /// key, it will be replaced with the specified value.
+      HttpRequest& setHeader( const QString& key, const QString& value );
+
+      /// Return the requests parameters as a string.
+      /// \b Note: A leading ? symbol will not be added, which is required
+      /// for GET requests.  Calls must add if making a GET request.
+      const QString getParamters() const;
+
+      /// Return a constant iterator to the beginning of the headers
+      /// map.
+      Iterator beginHeaders() const { return headers.begin(); }
+
+      /// Return a constant iterator to the beginning of the headers
+      /// map.
+      Iterator endHeaders() const { return headers.end(); }
+
+    private:
+      void toHex( unsigned value, int width, QString& output ) const;
+      QString encode( const QString& value ) const;
+
+    private:
+      static const QString ILLEGAL;
+
+      const QString uri;
+      QString body;
+      Map parameters;
+      Map headers;
+    };
+
+  } // namespace net
+} // namespace qsense
+
+#endif // QSENSE_NET_HTTPREQUEST_H
